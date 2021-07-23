59e41385a260f6daca9976efc8c0f8afab6f7171
Avisa-SEI <aap571420@gmail.com>
2021-07-23 16:31:01 -0700

    wip:

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Avisa Poshtkouhi',
+    description: 'Full Stack Portfolio',
+    copyright: 'This website is copyright 2021 Avisa Poshtkouhi'
+  }
 }

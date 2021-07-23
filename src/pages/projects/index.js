080ec47fe6f807ed15e03509776b08deb0d44d99
Avisa-SEI <aap571420@gmail.com>
2021-07-23 15:50:55 -0700

    wip: index page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import {portfolio} from '../../styles/projects.module.css'
+
+export default function Projects() {
+    return (
+        <Layout>
+        <div className={portfolio}>
+            <h2>Portofolio</h2>
+            <h3>Projects & Websites I've Created</h3>
+        </div>
+        </Layout>
+    )
+}

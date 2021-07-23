3bbf3c9f8a945a205608071fa4f94d2b18c0c879
Avisa-SEI <aap571420@gmail.com>
2021-07-23 15:49:30 -0700

    wip: Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
+import { Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import {header, btn} from '../styles/home.module.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+    <section className={header}>
+    <div>
+      <h2>Design</h2>
+      <h3>Develop & Deploy</h3>
+      <p>UX designer & web developer based in SanJose</p>
+      <Link className={btn} to="/projects">My Portfolio Projects</Link>
+    </div>
+    <img src="/banner.jpg" alt="banner" style={{maxWidth: '100%'}}/>
+    </section>
+    </Layout>
+  )
 }

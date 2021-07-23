ea4512cbe1fd766c7bde17b8312fa59ebfb33a74
Avisa-SEI <aap571420@gmail.com>
2021-07-23 15:50:15 -0700

    wip: components/Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Navbar from "./Navbar"
+import '../styles/global.css'
+
+export default function Layout({ children }) {
+  return (
+    <div className="Layout">
+      <Navbar />
+      <div className="content">
+        {children}
+        <div>
+          <footer>
+            <p>Copyright 2021 Web Avisa</p>
+          </footer>
+        </div>
+      </div>
+    </div>
+  )
+}

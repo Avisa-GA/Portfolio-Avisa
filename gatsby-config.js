d08dbfb947bfc77a2af7eb46c195fa7e447804c2
Avisa-SEI <aap571420@gmail.com>
2021-07-23 17:21:25 -0700

    wip: install gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,32 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    // You can have multiple instances of this plugin
+    // to read source nodes from different locations on your
+    // filesystem.
+    //
+    // The following sets up the Jekyll pattern of having a
+    // "pages" directory for Markdown files and a "data" directory
+    // for `.json`, `.yaml`, `.csv`.
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `notes`,
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: 'Avisa Poshtkouhi',
-    description: 'Full Stack Portfolio',
+    description: 'full-stack Software Engineer',
     copyright: 'This website is copyright 2021 Avisa Poshtkouhi'
   }
 }

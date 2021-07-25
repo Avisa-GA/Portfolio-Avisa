de65c28b252e7b2a615176dfd935c3c71aceb69b
Avisa-SEI <aap571420@gmail.com>
2021-07-25 10:53:05 -0700

    wip:

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function About() {
-    return (
-        <Layout>
-        <div>
-            <h1>About</h1>
-            <p>I'm a Full Stack Engineer / Developer with bachelor degree of software engineering and with the Background of iOS development .
-
-I am a quick learner, who can absorb new ideas quickly and can communicate clearly and effectively.
-
-I have the ability to turn complex idea into clear and compelling stories . Motivated by the desire to help people tell dynamic and captivating narratives that harness the interactive qualities of the web.</p>
-        </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div>
+        <h1>About</h1>
+        <p>
+          I'm a Full Stack Engineer / Developer with bachelor degree of software
+          engineering and with the Background of iOS development .
+        </p>
+        <p>
+          I am a quick learner, who can absorb new ideas quickly and can
+          communicate clearly and effectively.
+        </p>
+        <p>
+          I have the ability to turn complex idea into clear and compelling
+          stories . Motivated by the desire to help people tell dynamic and
+          captivating narratives that harness the interactive qualities of the
+          web.
+        </p>
+      </div>
+    </Layout>
+  );
 }

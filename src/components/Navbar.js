8f12e02301b54b238e2e210ab9c9678a8ee7845e
Avisa-SEI <aap571420@gmail.com>
2021-07-25 09:43:27 -0700

    wip: delete profile image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
     return (
         <nav>
           <div className="title" style={{display:"flex", marginBottom: "10%"}}>
-            <img src="/profile.jpg" alt="Avisa" style={{maxWidth: "48px",maxHeight: "48px", borderRadius: "50%", marginTop: "-3%"}}/>
+            {/* <img src="/profile.jpg" alt="Avisa" style={{maxWidth: "48px",maxHeight: "48px", borderRadius: "50%", marginTop: "-3%"}}/> */}
             <h1 style={{marginLeft: "1%"}}>{title}</h1>
           </div>
             <div className="links">

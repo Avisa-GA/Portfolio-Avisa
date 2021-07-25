f5a29c27aecdd82d94226e20f427e20f47e8e752
Avisa-SEI <aap571420@gmail.com>
2021-07-25 10:21:18 -0700

    wip: change intro in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ export default function Home({data}) {
     <Layout>
       <section className={header}>
         <div>
-          <h2>Design</h2>
-          <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in SanJose</p>
+          <h2>Software Engineer</h2>
+          <h3>full stack</h3>
+          <p>software developer based in SanJose</p>
           <Link className={btn} to="/projects">
             My Portfolio Projects
           </Link>

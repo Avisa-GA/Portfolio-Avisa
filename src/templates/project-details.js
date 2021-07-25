3c49d13f16ec5aee49bdbeff8dfbbbd7eed129b8
Avisa-SEI <aap571420@gmail.com>
2021-07-25 09:21:30 -0700

    wip:

---
+++ src/templates/project-details.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Img from "gatsby-image";
+import {
+  featured,
+  details,
+  htmlTemp,
+} from "../styles/project-details-module.css";
+
+export default function ProjectDetails({ data }) {
+
+  const { html } = data.markdownRemark;
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <div className={details}>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={featured}>
+          <Img fluid={featuredImg.childImageSharp.fluid} />
+        </div>
+        <div>
+        <div className={htmlTemp} dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+

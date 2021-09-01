d20434c6d51040cc7b264fcbcc26966db1c802bd
Avisa-SEI <aap571420@gmail.com>
2021-09-01 12:12:00 -0700

    wip: fix syntax

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,59 +1,68 @@
-import { graphql, Link } from 'gatsby'
-import React from 'react'
-import Layout from '../../components/Layout'
-import {portfolio, projects} from '../../styles/projects.module.css'
-import Img from 'gatsby-image';
+import { graphql, Link } from "gatsby"
+import React from "react"
+import Layout from "../../components/Layout"
+import { portfolio, projects } from "../../styles/projects.module.css"
+import Img from "gatsby-image"
 
-export default function Projects({data}) {
-    // console.log(data);
-    const projectsList = data.projects.nodes;
-    const contact = data.contact.siteMetadata.contact;
-    return (
-        <Layout>
-        <div className={portfolio}>
-            <h2>Portofolio</h2>
-            <h3>Projects & Websites I've Created</h3>
-            <div className={projects}>
-                {projectsList.map(project => {
-                   return ( <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
-                       <div>
-                          <Img fluid={project.frontmatter.thumb.childImageSharp.fluid}/>
-                           <h3>{project.frontmatter.title}</h3>
-                           <p>{project.frontmatter.stack}</p>
-                       </div>
-                   </Link>);
-                })}
-            </div>
-            <p>My Email {contact}</p>
+export default function Projects({ data }) {
+  // console.log(data);
+  const projectsList = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+  return (
+    <Layout>
+      <div className={portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={projects}>
+          {projectsList.map(project => {
+            return (
+              <Link
+                to={"/projects/" + project.frontmatter.slug}
+                key={project.id}
+              >
+                <div>
+                  <Img
+                    fluid={project.frontmatter.thumb.childImageSharp.fluid}
+                  />
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            )
+          })}
         </div>
-        </Layout>
-    )
+        <p>My Email {contact}</p>
+      </div>
+    </Layout>
+  )
 }
 
 // export page query
 export const query = graphql`
-query ProjectsPage {
-  projects: allMarkdownRemark(sort: {fields: frontmatter___title, order: ASC}) {
-    nodes {
-      frontmatter {
-        slug
-        stack
-        title
-        thumb {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
+  query ProjectsPage {
+    projects: allMarkdownRemark(
+      sort: { fields: frontmatter___title, order: ASC }
+    ) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
             }
           }
         }
+        id
       }
-      id
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
     }
   }
-  contact: site {
-    siteMetadata {
-      contact
-    }
-  }
-} 
-`;
+`

5111be69add1ec074527081719edf7a3de1ee7a6
Avisa Poshtkouhi <aap571420@gmail.com>
2022-10-04 12:36:21 -0700

    feat: add some css on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,42 @@
-import { graphql, Link } from "gatsby";
-import React from "react";
-import Layout from "../components/Layout";
-import { header, btn } from "../styles/home.module.css";
-import Img from 'gatsby-image';
+import { graphql, Link } from "gatsby"
+import React from "react"
+import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
+import Img from "gatsby-image"
 
-export default function Home({data}) {
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={header}>
-        <div style={{ display:'flex', justifyContent:'space-between' }} >
+        <div>
           <h2>Software Engineer</h2>
           <h3>full stack</h3>
           <p>based in San Jose</p>
-          <Link className={btn} to="https://www.linkedin.com/in/avisa-a-poshtkouhi/">
-            LinkedIn
-          </Link>
-          <Link className={btn} to="https://generalassemb.ly/instructors/avisa-poshtkouhi/28400">
-            General Assembly
-          </Link>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <Link
+              className={btn}
+              to="https://www.linkedin.com/in/avisa-a-poshtkouhi/"
+            >
+              LinkedIn
+            </Link>
+            <Link
+              className={btn}
+              to="https://generalassemb.ly/instructors/avisa-poshtkouhi/28400"
+            >
+              General Assembly
+            </Link>
+          </div>
         </div>
-        <Img
-          fluid={data.file.childImageSharp.fluid}
-        />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
-  );
+  )
 }
 
 export const query = graphql`
@@ -37,4 +49,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
